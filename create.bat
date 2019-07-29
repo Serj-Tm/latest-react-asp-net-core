996d906d62b64b9b1da199f3f5fc304220fd2ad5
- <->
2019-07-29 23:33:43 +0300

    rename serviceworker to js (fix previous changing)

--- create.bat
+++ create.bat
@@ -17,8 +17,8 @@ copy /Y %rname%\README.md %name%\ClientApp\
 copy /Y %rname%\public\index.html %name%\ClientApp\public\
 copy /Y %rname%\public\manifest.json %name%\ClientApp\public\
 
-rem copy /Y %rname%\src\serviceWorker.ts %name%\ClientApp\src\
-rem del %name%\ClientApp\src\registerServiceWorker.js
+copy /Y %rname%\src\serviceWorker.ts %name%\ClientApp\src\serviceWorker.js
+del %name%\ClientApp\src\registerServiceWorker.js
 ren %name%\ClientApp\src\components\Home.js Home.tsx
 ren %name%\ClientApp\src\components\NavMenu.js NavMenu.tsx
 
